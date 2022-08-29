a8302a95040424e98c593eb9bf415105c30ea4c8
Ledaryy <andrew.ledary@gmail.com>
2022-08-29 01:36:26 +0100

    Experementing with libraries and fetch, implemented time zone logic

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import moment from 'moment';
 
 // Remember to rename these classes and interfaces!
 
@@ -17,65 +18,79 @@ export default class MyPlugin extends Plugin {
 		await this.loadSettings();
 
 		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
+		const ribbonIconEl = this.addRibbonIcon('sync', 'Sample Plugin', (evt: MouseEvent) => {
 			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
+			const timeInput = "2022-08-29"
+			const currentTime = new Date()
+
+			// const timeStart = 
+			const timeStart = new Date(Date.parse(timeInput + "T00:00:00Z"));
+			// const correctDate = 
+			const correctTime = new Date(timeStart.getTime() + (currentTime.getTimezoneOffset() * 60 * 1000));
+			const formattedDate = moment(correctTime).format('YYYY-MM-DD'); 
+			const formattedTime = moment(correctTime).format('HH:mm');
+			console.log(formattedDate)
+			console.log(formattedTime)
+
+
+			console.log(currentTime.toUTCString())
+			console.log(timeStart.toUTCString())
+
+			console.log(correctTime.toUTCString())
+
+			// console.log(timeStart.toUTCString());
+			
+			async function getTasks() {
+				const url = `https://api.todoist.com/sync/v8/completed/get_all?since=` + formattedDate + `T` + formattedTime + `&until=` + timeInput + `T23:59`;
+				console.log(url)
+				const response = await fetch(url, {
+					headers: {
+						"Authorization": ""
+					}
+				}).then(function (response) {
+					// console.log(response.json());
+					return response.json();
+				});
+				console.log(response);
+			}
+			getTasks();
+
+			new Notice("Loading tasks...");
 		});
 		// Perform additional things with the ribbon
 		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
-
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
 		// This adds an editor command that can perform some operation on the current editor instance
 		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
+			id: 'todoist-fetch-completed-tasks',
+			name: 'Fetch completed tasks',
+			callback: async () => {
+				console.log("Trying to fetch API")
+				// if (this.api != null) {
+				//   debug("Refreshing metadata");
+				//   const result = await this.api.fetchMetadata();
+
+				//   if (result.isErr()) {
+				// 	console.error(result.unwrapErr());
+				//   }
+				// }
+				// const url = `https://api.todoist.com/sync/v8/completed/get_all?since=` + date + `T00:00&until=` + date + `T23:59`;
+				const url = `https://api.todoist.com/sync/v8/completed/get_all?since=` + "2022-08-29" + `T00:00&until=` + "2022-08-29" + `T23:59`;
+				const response = await fetch(url, {
+					headers: {
+						"Authorization": ""
 					}
-
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
+				}).then(function (response) {
+					// console.log(response.json());
+					return response.json();
+				});
+				console.log(response);
+				// const repositories = await response.json();
+				// return repositories;
+			},
 		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
 	}
 
 	onunload() {
@@ -97,12 +112,12 @@ class SampleModal extends Modal {
 	}
 
 	onOpen() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.setText('Woah!');
 	}
 
 	onClose() {
-		const {contentEl} = this;
+		const { contentEl } = this;
 		contentEl.empty();
 	}
 }
@@ -116,11 +131,11 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+		containerEl.createEl('h2', { text: 'Settings for my awesome plugin.' });
 
 		new Setting(containerEl)
 			.setName('Setting #1')
